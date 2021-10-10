c0167374e097fc7e511d7752029085dfc1a1a67a
Stalker-Return <eduard.babyshenko@google.com>
2021-10-10 09:15:21 +0300

    Dayly upload

---
+++ scripts/i3/umount_i3.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+# a dmenu prompt to umount usb drives
+# Dependancies: dmenu, pgrep, notify-send
+#
+exlusionregex="\(/boot\|/home\|/\)$"
+drives=$(lsblk -lp | grep "t /" | grep -v "$exlusionregex" | awk '{print $1, "(" $4 ")", "on", $7}')
+[[ -z $drives ]] && exit 1
+#
+chosen=$(echo "$drives" | dmenu -i -p "Choose the Drive to umount" | awk '{print $1}')
+[[ -z $chosen ]] && exit 1
+#
+sudo umount $chosen && pgrep -x dunst && notify-send "$chosen mounted"
