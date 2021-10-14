e8d10fdb9dfab3c27826812f5b2b00c9a10baeed
Stalker-Return <eduard.babyshenko@google.com>
2021-10-15 00:00:05 +0300

    Dayly upload

--- bin/aur_update.sh
+++ bin/aur_update.sh
@@ -4,7 +4,7 @@
 	[[ ! -f $checkfile ]] && echo -e "File $(basename -- "$checkfile") does not exist. Press any key to continue." && read -rsn1 && exit 5 || source $checkfile;
 # Full system update
 function updatenow() {
-	sudo pacman -Syu --noconfirm --logfile $HOME/.local/share/log-files/sys_pacman.log
+	yay -Sua --noconfirm
 	exitcode=$?
 	exitcontrol
 }
@@ -13,12 +13,12 @@ function notifynownow() {
 	iconerr=/home/ed/.local/share/icons/status/dialog-warning.svg
 	iconst=/home/ed/.local/share/icons/status/trophy-gold.svg
 
-	echo "Exit code = $exitcode | $codedescription | $(date) | Full System Update has completed." \
-	>> $HOME/.local/share/log-files/sys_update.log
+	echo "Exit code = $exitcode | $codedescription | $(date) | AUR packages update has completed." \
+	>> $HOME/.local/share/log-files/aur_update.log
 
 	case $exitcode in
-	 0)	notify-send -u normal -t 3000 'Full System Update' 'Result: SUCCESS' -i $iconst && exit 0 ;;
-	 *)	notify-send -u critical -t 3000 'Full System Update' 'Result: ERROR' -i $iconerr && exit 1 ;;
+	 0)	notify-send -u normal -t 3000 'AUR packages update' 'Result: SUCCESS' -i $iconst && exit 0 ;;
+	 *)	notify-send -u critical -t 3000 'AUR packages update' 'Result: ERROR' -i $iconerr && exit 1 ;;
 	esac
 }
 #
