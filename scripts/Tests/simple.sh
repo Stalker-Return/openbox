274b628ee569fd86f4895220827cca406285c150
Stalker-Return <eduard.babyshenko@google.com>
2021-10-31 00:00:21 +0300

    Dayly upload

--- scripts/Tests/simple.sh
+++ scripts/Tests/simple.sh
@@ -1,23 +1,17 @@
 #!/bin/bash
-source /home/ed/scripts/Tests/try_func.sh
+# xterm -bg blue4 -fg snow -T Wi-Fi -geometry 43x20+1103+479 -e 'tput civis; echo -e "\033[5mAvailable Wi-Fi\033[0m"; tput setaf 10; sudo iw dev wlp3s0 scan | grep SSID; echo -e "\n"; tput setaf 15; read -rsn1 -p "Press any key to exit... "'
 
-spin() {
-  local i=0
-  local sp='/-\|'
-  local n=${#sp}
-  printf ' '
-  sleep 0.1
-  while true; do
-    printf '\b%s' "${sp:i++%n:1}"
-    sleep 0.1
-  done
-}
-
-pingnow() {
-ping google.com
-}
-
-first='abc'
-second='abcdef'
-p_append $first $second
-echo $second
+tput civis
+echo -e "\033[5mAvailable Wi-Fi\033[0m"
+tput setaf 10
+sudo iw dev wlp3s0 scan | grep SSID
+echo -e "\n"
+tput setaf 15
+#read -rsn1 -p "Press any key to exit... "
+read -n 1 -r -p "'w' to run nmtui or 'x | ESC' for exit: " optionA
+  case $optionA in
+    w|W ) runreflector ;;
+    $'\e' ) clear; tput cvvis; exit;;		# Exit on ESC - button
+    x|X ) clear; tput cvvis; exit;;
+    * ) invalid ;;
+  esac
